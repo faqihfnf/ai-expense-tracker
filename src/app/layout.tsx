74650b36c91a9573f7136e7c6a944b69670f97f2
faqihfnf <faqih.fnf@gmail.com>
2025-07-28 17:15:29 +0700

    add navbar components, home, about and contact page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
+import { Inter, Poppins } from "next/font/google";
 import Navbar from "@/components/Navbar";
+import { ThemeProvider } from "@/context/ThemeContext";
+import ClerkThemeProvider from "@/components/ClerkThemeProvider";
+import Footer from "@/components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ subsets: ["latin"], weight: "400" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,14 +20,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          <Navbar />
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
+    <ThemeProvider>
+      <ClerkThemeProvider>
+        <html lang="en">
+          <body className={`${inter.className} ${poppins.className}`}>
+            <Navbar />
+            {children}
+            <Footer />
+          </body>
+        </html>
+      </ClerkThemeProvider>
+    </ThemeProvider>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
+import Guest from "@/components/Guest";
+import { currentUser } from "@clerk/nextjs/server";
+
+export default async function Home() {
+  const user = await currentUser();
+
+  if (!user) {
+    return <Guest />;
+  }
   return <div className="">Home</div>;
 }
